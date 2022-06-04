c03d54efac1e66334afe9319057286239aca6477
Dim <eloctro@tut.by>
2022-06-05 00:47:46 +0300

    feat: src/fs/copy.js task implement

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,46 @@
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const copy = async () => {
-    // Write your code here 
+  fs.readdir(path.join(__dirname), (err, files) => {
+    if (err) {
+      throw err;
+    }
+
+    try {
+      if (files.includes("files-copy") || !files.includes("files")) {
+        throw new Error();
+      } else {
+        fs.mkdir(path.join(__dirname, "files-copy"), (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+
+        fs.readdir(path.join(__dirname, "files"), (err, items) => {
+          if (err) {
+            throw err;
+          }
+
+          items.forEach((elem) => {
+            fs.copyFile(
+              path.join(__dirname, "files", `${elem}`),
+              path.join(__dirname, "files-copy", `${elem}`),
+              (err) => {
+                if (err) throw err;
+              }
+            );
+          });
+        });
+      }
+    } catch (err) {
+      console.error("FS operation failed");
+    }
+  });
 };
+
+copy();

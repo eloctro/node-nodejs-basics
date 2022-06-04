ab7d2187a25098a33c0a20456a82741a7e210150
Dim <eloctro@tut.by>
2022-06-04 18:55:39 +0300

    feat: src/fs/create.js task implement

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,27 @@
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const text = "I am fresh and young";
+const pathToDir = path.join(__dirname, "files");
+
 export const create = async () => {
-    // Write your code here 
+  fs.readdir(pathToDir, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      if (file === "fresh.txt") {
+        console.error(`FS operation failed`);
+      }
+    });
+  });
+
+  fs.writeFile(path.join(pathToDir, "fresh.txt"), text, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
 };
+
+create();

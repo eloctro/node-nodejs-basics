9bc1f56035cacfca298dcad78c956789f1af66d5
Dim <eloctro@tut.by>
2022-06-05 01:11:47 +0300

    feat: src/fs/rename.js task implement

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,36 @@
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const rename = async () => {
-    // Write your code here 
+  fs.readdir(path.join(__dirname, "files"), (err, items) => {
+    if (err) {
+      throw err;
+    }
+    try {
+      if (
+        items.includes("properFilename.md") ||
+        !items.includes("wrongFilename.txt")
+      ) {
+        throw new Error();
+      } else {
+        fs.rename(
+          path.join(__dirname, "files", "wrongFilename.txt"),
+          path.join(__dirname, "files", "properFilename.md"),
+          (err) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+      }
+    } catch (err) {
+      console.error("FS operation failed");
+    }
+  });
 };
+
+rename();

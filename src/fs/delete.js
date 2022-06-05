db96819aec35de382cfad2fe6b9128067a5cfc85
Dim <eloctro@tut.by>
2022-06-05 11:14:14 +0300

    feat: src/fs/delete.js task implement

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,30 @@
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const remove = async () => {
-    // Write your code here 
+  fs.readdir(path.join(__dirname, "files"), (err, files) => {
+    if (err) {
+      throw err;
+    }
+
+    try {
+      if (!files.includes("fileToRemove.txt")) {
+        throw new Error();
+      } else {
+        fs.rm(path.join(__dirname, "files", "fileToRemove.txt"), (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+    } catch (err) {
+      console.error(`FS operation failed`);
+    }
+  });
 };
+
+remove();

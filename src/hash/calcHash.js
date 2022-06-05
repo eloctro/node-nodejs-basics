943991f1403f827de517c4674252c3a08e1ad18f
Dim <eloctro@tut.by>
2022-06-05 21:00:25 +0300

    feat: src/hash/calcHash.js task implement

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const { createHash } = await import("crypto");
+
 export const calculateHash = async () => {
-    // Write your code here 
+  fs.readFile(
+    path.join(__dirname, "files", "fileToCalculateHashFor.txt"), (err, data) => {
+      if (err) {
+        throw err;
+      }
+      const hash = createHash("sha256").update(data).digest("hex");
+      console.log(hash);
+    }
+  );
+  
 };
+
+calculateHash();
